537ba148de6af79605180ae612d7075bdf8344b3
Devotion <letsmaildipak@gmail.com>
2024-01-12 00:02:16 +0530

    Implemented adding a new entry using innerHTML, learned about template literals, using # + dr.value for Id,  learned querySelectorAll() which returns a NodeList ( array like object)  HTMLString

--- CalorieCounter/script.js
+++ CalorieCounter/script.js
@@ -15,3 +15,22 @@ function isInvalidInput(str) {
   const regex = /\d+e\d+/i;
   return str.match(regex);
 }
+
+function addEntry() {
+  const targetInputContainer = document.querySelector(
+    `#${entryDropdown.value} .input-container`
+  );
+  const entryNumber =
+    targetInputContainer.querySelectorAll('input[type="text"]').length;
+  const HTMLString = `
+  <label for="${entryDropdown.value}-${entryNumber}-name">Entry ${entryNumber} Name</label>
+  <input type="text" id="${entryDropdown.value}-${entryNumber}-name" placeholder="Name" />
+  <label for="${entryDropdown.value}-${entryNumber}-calories">Entry ${entryNumber} Calories</label>
+  <input
+    type="number"
+    min="0"
+    id="${entryDropdown.value}-${entryNumber}-calories"
+    placeholder="Calories"
+  />`;
+  targetInputContainer.innerHTML += HTMLString;
+}
