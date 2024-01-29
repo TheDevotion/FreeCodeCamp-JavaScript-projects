b092495613969a6717b3ec3effd1296909ca8f58
Devotion <letsmaildipak@gmail.com>
2024-01-29 17:10:12 +0530

    play top song of playlist, and pause song using pause button

--- MusicPlayer/script.js
+++ MusicPlayer/script.js
@@ -101,12 +101,19 @@ const playSong = (id) => {
   audio.play();
 };
 
+const pauseSong = () => {
+  userData.songCurrentTime = audio.currentTime;
+
+  playButton.classList.remove("playing");
+  audio.pause();
+};
+
 const renderSongs = (array) => {
   const songsHTML = array
     .map((song) => {
       return `
       <li id="song-${song.id}" class="playlist-song">
-      <button class="playlist-song-info">
+      <button class="playlist-song-info" onclick="playSong(${song.id})">
           <span class="playlist-song-title">${song.title}</span>
           <span class="playlist-song-artist">${song.artist}</span>
           <span class="playlist-song-duration">${song.duration}</span>
@@ -130,5 +137,7 @@ playButton.addEventListener("click", () => {
     playSong(userData?.currentSong.id);
   }
 });
+
+pauseButton.addEventListener("click", pauseSong);
 
 renderSongs(userData?.songs);
