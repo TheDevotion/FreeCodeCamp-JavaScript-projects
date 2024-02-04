de60ff7694c21f425ea916422aced4f6c48e2e69
Devotion <letsmaildipak@gmail.com>
2024-02-04 21:16:26 +0530

    html done and some css done

--- PalindromeChecker/script.js
+++ PalindromeChecker/script.js
@@ -1,5 +1,7 @@
+// copy of: https://palindrome-checker.freecodecamp.rocks/
+
 const checkButton = document.getElementById("check-btn");
 
 const textInput = document.getElementById("text-input");
 
-alert("Please Input a value");
+// alert("Please Input a value");
