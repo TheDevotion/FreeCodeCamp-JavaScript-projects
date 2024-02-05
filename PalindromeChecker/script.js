3209e9a270db0ed97c2cae955db498ce13f8b7cc
Devotion <letsmaildipak@gmail.com>
2024-02-05 23:55:18 +0530

    tried some logic

--- PalindromeChecker/script.js
+++ PalindromeChecker/script.js
@@ -4,4 +4,18 @@ const checkButton = document.getElementById("check-btn");
 
 const textInput = document.getElementById("text-input");
 
-// alert("Please Input a value");
+const str = new String(textInput.value);
+
+const revStr = str.split().reverse().join();
+
+checkButton.addEventListener("click", (str) => {
+  if (str === null) {
+    alert("Please Input a value");
+  } else {
+    if (str == revStr) {
+      alert("is palindrome");
+    } else {
+      alert("not palindrome");
+    }
+  }
+});
