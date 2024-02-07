98693d88548d06ffb46b8a9552b4e8daa720f6ff
Devotion <letsmaildipak@gmail.com>
2024-02-08 00:14:42 +0530

    worked with regex and passed all test cases.

--- PalindromeChecker/script.js
+++ PalindromeChecker/script.js
@@ -5,13 +5,20 @@ const result = document.getElementById("result");
 
 checkButton.addEventListener("click", displayResult);
 
-// function to reverse the string:
-function reverseString(str) {
-  return Array.from(str).reverse().join("");
+// function to reverse the clean string:
+function reverseString(cleanString) {
+  return Array.from(cleanString).reverse().join("");
 }
 // function to check if given string is palindrome or not.
 function isPalindrome(str) {
-  if (str === reverseString(str)) {
+  const cleanString = getCleanString(str);
+
+  console.log(cleanString);
+
+  const revString = reverseString(cleanString);
+  console.log(revString);
+
+  if (cleanString === revString) {
     return true;
   }
   return false;
@@ -30,3 +37,19 @@ function displayResult() {
     result.innerHTML = `${str} is not a palindrome`;
   }
 }
+
+// function to clean the string from spaces, underscore and numbers.
+// also removing , and .
+// also converting it to lowercase.
+
+function getCleanString(str) {
+  return str.replace(/[^a-zA-Z0-9]+/g, "").toLowerCase();
+}
+
+// /[^a-zA-Z0-9]+/g: This regular expression pattern matches any
+//sequence of characters that are not letters or digits. The ^ inside
+// the square brackets [^...] negates the character set,
+// meaning it matches any character that is not within the specified range.
+//The + quantifier matches one or more occurrences of the characters.
+// The g flag at the end indicates that the replacement
+//should be done globally, i.e., for all occurrences in the string.
