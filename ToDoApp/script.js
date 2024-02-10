791161673f66e19008727d77f8b18c02a8237d60
Devotion <letsmaildipak@gmail.com>
2024-02-10 22:55:06 +0530

    separating adding task to array and to dom to maintain readability, creating a reset function which wiil reset the input values, and the current task will get empty also hides the taskForm ofr each submit adding the taskData to DOM and then reset

--- ToDoApp/script.js
+++ ToDoApp/script.js
@@ -13,24 +13,7 @@ const descriptionInput = document.getElementById("description-input");
 const taskData = [];
 let currentTask = {};
 
-openTaskFormBtn.addEventListener("click", () =>
-  taskForm.classList.toggle("hidden")
-);
-
-closeTaskFormBtn.addEventListener("click", () => {
-  confirmCloseDialog.showModal();
-});
-
-cancelBtn.addEventListener("click", () => confirmCloseDialog.close());
-
-discardBtn.addEventListener("click", () => {
-  confirmCloseDialog.close();
-  taskForm.classList.toggle("hidden");
-});
-
-taskForm.addEventListener("submit", (e) => {
-  e.preventDefault();
-
+const addOrUpdateTask = () => {
   const dataArrIndex = taskData.findIndex((item) => item.id === currentTask.id);
   const taskObj = {
     id: `${titleInput.value.toLowerCase().split(" ").join("-")}-${Date.now()}`,
@@ -38,4 +21,55 @@ taskForm.addEventListener("submit", (e) => {
     date: dateInput.value,
     description: descriptionInput.value,
   };
+
+  if (dataArrIndex === -1) {
+    taskData.unshift(taskObj);
+  }
+};
+
+const reset = () => {
+  titleInput.value = "";
+  dateInput.value = "";
+  descriptionInput.value = "";
+  taskForm.classList.toggle("hidden");
+  currentTask = {};
+};
+
+openTaskFormBtn.addEventListener("click", () =>
+  taskForm.classList.toggle("hidden")
+);
+
+closeTaskFormBtn.addEventListener("click", () => {
+  const formInputsContainValues =
+    titleInput.value || dateInput.value || descriptionInput.value;
+  if (formInputsContainValues) {
+    confirmCloseDialog.showModal();
+  } else {
+    reset();
+  }
+});
+
+cancelBtn.addEventListener("click", () => confirmCloseDialog.close());
+
+discardBtn.addEventListener("click", () => {
+  confirmCloseDialog.close();
+  reset();
+});
+
+taskForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  taskData.forEach(({ id, title, date, description }) => {
+    tasksContainer.innerHTML += `
+        <div class="task" id="${id}">
+          <p><strong>Title:</strong> ${title}</p>
+          <p><strong>Date:</strong> ${date}</p>
+          <p><strong>Description:</strong> ${description}</p>
+          <button type="button" class="btn">Edit</button>
+          <button type="button" class="btn">Delete</button>
+        </div>
+      `;
+  });
+
+  reset();
 });
