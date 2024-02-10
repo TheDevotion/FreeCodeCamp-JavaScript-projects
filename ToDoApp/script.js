a0aa439c24095b2c2e80e7d40b97202d0fcffc83
Devotion <letsmaildipak@gmail.com>
2024-02-10 22:25:58 +0530

    add toggle hidden classlist to task form btn, anding showModal, close(), after submit preveting default and then finding index if creating task object for unique id use date.now and get all othervalues by .value

--- ToDoApp/script.js
+++ ToDoApp/script.js
@@ -12,3 +12,30 @@ const descriptionInput = document.getElementById("description-input");
 
 const taskData = [];
 let currentTask = {};
+
+openTaskFormBtn.addEventListener("click", () =>
+  taskForm.classList.toggle("hidden")
+);
+
+closeTaskFormBtn.addEventListener("click", () => {
+  confirmCloseDialog.showModal();
+});
+
+cancelBtn.addEventListener("click", () => confirmCloseDialog.close());
+
+discardBtn.addEventListener("click", () => {
+  confirmCloseDialog.close();
+  taskForm.classList.toggle("hidden");
+});
+
+taskForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const dataArrIndex = taskData.findIndex((item) => item.id === currentTask.id);
+  const taskObj = {
+    id: `${titleInput.value.toLowerCase().split(" ").join("-")}-${Date.now()}`,
+    title: titleInput.value,
+    date: dateInput.value,
+    description: descriptionInput.value,
+  };
+});
