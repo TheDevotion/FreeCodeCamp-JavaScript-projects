57ec48e7146ac5124bbf027579f7de65bbeb238e
Devotion <letsmaildipak@gmail.com>
2024-02-12 02:09:17 +0530

    declared inputs, quotients and remainders array, remainders array reversed is our binary number, we used if input is greater than zeor keep dividing it by 2 and then store  remainder of mod 2 in remeinder and then set input as quotient again.

--- DecimalToBinaryConvertor/script.js
+++ DecimalToBinaryConvertor/script.js
@@ -2,11 +2,41 @@ const numberInput = document.getElementById("number-input");
 const convertBtn = document.getElementById("convert-btn");
 const result = document.getElementById("result");
 
-const checkUserInput = () => {
-  if (!numberInput.value) {
+const decimalToBinary = (input) => {
+  const inputs = [];
+  const quotients = [];
+  const remainders = [];
+
+  if (input === 0) {
+    result.innerText = "0";
+    return;
   }
 
-  console.log(numberInput.value);
+  while (input > 0) {
+    const quotient = Math.floor(input / 2);
+    const remainder = input % 2;
+
+    inputs.push(input);
+    quotients.push(quotient);
+    remainders.push(remainder);
+    input = quotient;
+  }
+
+  console.log("Inputs: ", inputs);
+  console.log("Quotients: ", quotients);
+  console.log("Remainders: ", remainders);
+
+  result.innerText = remainders.reverse().join("");
+};
+
+const checkUserInput = () => {
+  if (!numberInput.value || isNaN(parseInt(numberInput.value))) {
+    alert("Please provide a decimal number");
+    return;
+  }
+
+  decimalToBinary(parseInt(numberInput.value));
+  numberInput.value = "";
 };
 
 convertBtn.addEventListener("click", checkUserInput);
@@ -16,3 +46,4 @@ numberInput.addEventListener("keydown", (e) => {
     checkUserInput();
   }
 });
+``;
