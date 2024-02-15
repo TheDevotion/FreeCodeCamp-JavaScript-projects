8b55a10a9ccec6088fab1c7b546d7a75e76eb6af
Devotion <letsmaildipak@gmail.com>
2024-02-15 18:05:29 +0530

    Implemented Selection sort

--- NumberSorter/script.js
+++ NumberSorter/script.js
@@ -7,7 +7,7 @@ const sortInputArray = (event) => {
     ...document.getElementsByClassName("values-dropdown"),
   ].map((dropdown) => Number(dropdown.value));
 
-  const sortedValues = bubbleSort(inputValues);
+  const sortedValues = selectionSort(inputValues);
 
   updateUI(sortedValues);
 };
@@ -28,6 +28,24 @@ const bubbleSort = (array) => {
         array[j + 1] = temp;
       }
     }
+  }
+
+  return array;
+};
+
+const selectionSort = (array) => {
+  for (let i = 0; i < array.length; i++) {
+    let minIndex = i;
+
+    for (let j = i + 1; j < array.length; j++) {
+      if (array[j] < array[minIndex]) {
+        minIndex = j;
+      }
+    }
+
+    const temp = array[i];
+    array[i] = array[minIndex];
+    array[minIndex] = temp;
   }
 
   return array;
