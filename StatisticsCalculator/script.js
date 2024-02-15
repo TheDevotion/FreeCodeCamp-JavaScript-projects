9bfcfae652e5dec517a0ef4a9406d723f4c2d962
Devotion <letsmaildipak@gmail.com>
2024-02-16 01:35:40 +0530

    add getMean and getMedian used array.reduce

--- StatisticsCalculator/script.js
+++ StatisticsCalculator/script.js
@@ -1,5 +1,20 @@
+const getMean = (array) =>
+  array.reduce((acc, el) => acc + el, 0) / array.length;
+
+const getMedian = (array) => {
+  const sorted = array.sort((a, b) => a - b);
+  const median =
+    array.length % 2 === 0
+      ? (sorted[array.length / 2] + sorted[array.length / 2 - 1]) / 2
+      : sorted[array.length / 2];
+};
+
 const calculate = () => {
   const value = document.querySelector("#numbers").value;
   const array = value.split(/,\s*/g);
   const numbers = array.map((el) => Number(el)).filter((el) => !isNaN(el));
+
+  const mean = getMean(numbers);
+
+  document.querySelector("#mean").textContent = mean;
 };
