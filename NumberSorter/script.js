9dc3bccd812ab6bc7d2a9c1937efc32a25f2028a
Devotion <letsmaildipak@gmail.com>
2024-02-15 18:24:43 +0530

    completed the project, for the last part used .sort method but used a callback function witch returns a-b so that we get accurate  results

--- NumberSorter/script.js
+++ NumberSorter/script.js
@@ -7,7 +7,9 @@ const sortInputArray = (event) => {
     ...document.getElementsByClassName("values-dropdown"),
   ].map((dropdown) => Number(dropdown.value));
 
-  const sortedValues = insertionSort(inputValues);
+  const sortedValues = inputValues.sort((a, b) => {
+    return a - b;
+  });
 
   updateUI(sortedValues);
 };
