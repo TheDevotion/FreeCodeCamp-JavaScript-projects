42ccd45d789ce75e76014737fafc9460b20650dc
Devotion <letsmaildipak@gmail.com>
2024-02-18 00:51:29 +0530

    add range and variance function

--- StatisticsCalculator/script.js
+++ StatisticsCalculator/script.js
@@ -25,6 +25,21 @@ const getMode = (array) => {
   return mode.join(", ");
 };
 
+const getRange = (array) => {
+  return Math.max(...array) - Math.min(...array);
+};
+
+const getVariance = (array) => {
+  const mean = getMean(array);
+  const variance =
+    array.reduce((acc, el) => {
+      const difference = el - mean;
+      const squared = difference ** 2;
+      return acc + squared;
+    }, 0) / array.length;
+  return variance;
+};
+
 const calculate = () => {
   const value = document.querySelector("#numbers").value;
   const array = value.split(/,\s*/g);
@@ -33,8 +48,12 @@ const calculate = () => {
   const mean = getMean(numbers);
   const median = getMedian(numbers);
   const mode = getMode(numbers);
+  const range = getRange(numbers);
+  const variance = getVariance(numbers);
 
   document.querySelector("#mean").textContent = mean;
   document.querySelector("#median").textContent = median;
   document.querySelector("#mode").textContent = mode;
+  document.querySelector("#range").textContent = range;
+  document.querySelector("#variance").textContent = variance;
 };
