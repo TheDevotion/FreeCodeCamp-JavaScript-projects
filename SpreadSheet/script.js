02e1109f95bc8c1dccbffab40282f7b37984628b
Devotion <letsmaildipak@gmail.com>
2024-02-19 00:02:13 +0530

    learned about onload method, created range function and charRange function used Array, fill map charcodeat, fromcharcode methods

---
+++ SpreadSheet/script.js
@@ -0,0 +1,18 @@
+const range = (start, end) =>
+  Array(end - start + 1)
+    .fill(start)
+    .map((element, index) => element + index);
+const charRange = (start, end) =>
+  range(start.charCodeAt(0), end.charCodeAt(0)).map((code) =>
+    String.fromCharCode(code)
+  );
+
+window.onload = () => {
+  const container = document.getElementById("container");
+  const createLabel = (name) => {
+    const label = document.createElement("div");
+    label.className = "label";
+    label.textContent = name;
+    container.appendChild(label);
+  };
+};
