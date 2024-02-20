f7ab1bca75b58a1f7d86dd70f64fc986e4b657b9
Devotion <letsmaildipak@gmail.com>
2024-02-20 23:37:32 +0530

    added is even, sum average median function

--- SpreadSheet/script.js
+++ SpreadSheet/script.js
@@ -1,3 +1,17 @@
+const isEven = (num) => num % 2 === 0;
+const sum = (nums) => nums.reduce((acc, el) => acc + el, 0);
+const average = (nums) => sum(nums) / nums.length;
+
+const median = (nums) => {
+  const sorted = nums.slice().sort((a, b) => a - b);
+  const length = sorted.length;
+  const middle = length / 2 - 1;
+
+  return isEven(length)
+    ? average([sorted[middle], sorted[middle + 1]])
+    : sorted[Math.ceil(middle)];
+};
+
 const range = (start, end) =>
   Array(end - start + 1)
     .fill(start)
