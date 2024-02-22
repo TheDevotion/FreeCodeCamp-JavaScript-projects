dde75270829ce89f315f793d8d02f61403a3be0e
Devotion <letsmaildipak@gmail.com>
2024-02-22 23:13:30 +0530

    50 percent done. learned about currying i.e. implicitly returning the inner function where each function has only one parameter for modularity

--- SpreadSheet/script.js
+++ SpreadSheet/script.js
@@ -29,6 +29,10 @@ const charRange = (start, end) =>
 const evalFormula = (x, cells) => {
   const idToText = (id) => cells.find((cell) => cell.id === id).value;
   const rangeRegex = /([A-J])([1-9][0-9]?):([A-J])([1-9][0-9]?)/gi;
+  const rangeFromString = (num1, num2) => range(parseInt(num1), parseInt(num2));
+  const elemValue = (num) => (character) => idToText(character + num);
+  const addCharacters = (character1) => (character2) => (num) =>
+    charRange(character1, character2).map(elemValue(num));
 };
 
 window.onload = () => {
