9678c8c43d9e909d135f2ad43f032c1b0ccf9df2
Devotion <letsmaildipak@gmail.com>
2024-02-28 01:18:47 +0530

    basic js and some html changes

---
+++ TelephoneNumberValidator/script.js
@@ -0,0 +1,12 @@
+//getting all the elements:
+
+const userInput = document.getElementById("user-input");
+const result = document.getElementById("results-div");
+const checkBtn = document.getElementById("check-btn");
+const clearBtn = document.getElementById("clear-btn");
+
+checkBtn.addEventListener("click", () => {
+  if (userInput.value.length === 0) {
+    alert("Please provide a phone number");
+  }
+});
