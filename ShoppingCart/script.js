6e20b4dc551b70e7ab550b621903d35c2bf1c4e1
Devotion <letsmaildipak@gmail.com>
2024-02-29 00:39:34 +0530

    fixed CSS

--- ShoppingCart/script.js
+++ ShoppingCart/script.js
@@ -170,6 +170,7 @@ class ShoppingCart {
     cartSubTotal.textContent = `$${subTotal.toFixed(2)}`;
     cartTaxes.textContent = `$${tax.toFixed(2)}`;
     cartTotal.textContent = `$${this.total.toFixed(2)}`;
+
     return this.total;
   }
 }
