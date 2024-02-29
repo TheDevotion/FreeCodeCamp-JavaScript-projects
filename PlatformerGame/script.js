6e1390f048e265b81860a5800503d2f45a004bc0
Devotion <letsmaildipak@gmail.com>
2024-02-29 23:39:45 +0530

    taking all the elements, and creating a 2d canvas taking inner height width of canvas, setting gravity

---
+++ PlatformerGame/script.js
@@ -0,0 +1,12 @@
+const startBtn = document.getElementById("start-btn");
+const canvas = document.getElementById("canvas");
+const startScreen = document.querySelector(".start-screen");
+const checkpointScreen = document.querySelector(".checkpoint-screen");
+const checkpointMessage = document.querySelector(".checkpoint-screen > p");
+const ctx = canvas.getContext("2d");
+canvas.width = innerWidth;
+canvas.height = innerHeight;
+const gravity = 0.5;
+let isCheckpointCollisionDetectionActive = true;
+
+const proportionalSize = (size) => {};
