ee783aeecef0720cd80d8d1f4217ffb3d95ca532
Devotion <letsmaildipak@gmail.com>
2024-03-01 23:40:26 +0530

    took 10 more steps, learned about creating a Player class and having a proportional size function so that we have player according to the screen size

--- PlatformerGame/script.js
+++ PlatformerGame/script.js
@@ -9,4 +9,25 @@ canvas.height = innerHeight;
 const gravity = 0.5;
 let isCheckpointCollisionDetectionActive = true;
 
-const proportionalSize = (size) => {};
+const proportionalSize = (size) => {
+  return innerHeight < 500 ? Math.ceil((size / 500) * innerHeight) : size;
+};
+
+class Player {
+  constructor() {
+    this.position = {
+      x: proportionalSize(10),
+      y: proportionalSize(400),
+    };
+    this.velocity = {
+      x: 0,
+      y: 0,
+    };
+    this.width = proportionalSize(40);
+    this.height = proportionalSize(40);
+  }
+  draw() {
+    ctx.fillStyle = "#99c9ff";
+    ctx.fillRect(this.position.x, this.position.y, this.width, this.height);
+  }
+}
