10ee746f5d2c624be14fcc69caac919f74e70522
Devotion <letsmaildipak@gmail.com>
2024-03-03 00:51:30 +0530

    worked on update function

--- PlatformerGame/script.js
+++ PlatformerGame/script.js
@@ -35,5 +35,15 @@ class Player {
     this.draw();
     this.position.x += this.velocity.x;
     this.position.y += this.velocity.y;
+
+    if (this.position.y + this.height + this.velocity.y <= canvas.height) {
+      if (this.position.y < 0) {
+        this.position.y = 0;
+        this.velocity.y = gravity;
+      }
+      this.velocity.y += gravity;
+    } else {
+      this.velocity.y = 0;
+    }
   }
 }
