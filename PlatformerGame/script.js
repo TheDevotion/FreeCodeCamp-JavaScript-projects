021abef6c579f8eba10d826eac615f47f710c927
Devotion <letsmaildipak@gmail.com>
2024-03-04 23:24:39 +0530

    made the animate function using request animation frame API and left and right movement of player

--- PlatformerGame/script.js
+++ PlatformerGame/script.js
@@ -58,7 +58,31 @@ class Player {
 
 const player = new Player();
 
-const animate = () => {};
+const animate = () => {
+  requestAnimationFrame(animate);
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  player.update();
+
+  if (keys.rightKey.pressed && player.position.x < proportionalSize(400)) {
+    player.velocity.x = 5;
+  } else if (
+    keys.leftKey.pressed &&
+    player.position.x > proportionalSize(100)
+  ) {
+    player.velocity.x = -5;
+  } else {
+    player.velocity.x = 0;
+  }
+};
+
+const keys = {
+  rightKey: {
+    pressed: false,
+  },
+  leftKey: {
+    pressed: false,
+  },
+};
 
 const startGame = () => {
   canvas.style.display = "block";
