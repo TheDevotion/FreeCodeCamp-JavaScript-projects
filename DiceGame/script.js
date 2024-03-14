dc47ba25e2aaa1fa0da278157317487a463a8b69
Devotion <letsmaildipak@gmail.com>
2024-03-15 02:10:44 +0530

    wrote two line of code

--- DiceGame/script.js
+++ DiceGame/script.js
@@ -1,3 +1,3 @@
 const listOfAllDice = document.querySelectorAll(".die");
-
-const scoreInputs = document.querySelectorAll("#score-options");
+const scoreInputs = document.querySelectorAll("#score-options input");
+const scoreSpans = document.querySelectorAll("#score-options span");
