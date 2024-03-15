e2bcfad0bc01a438fe9f981919457d77d02209c8
Devotion <letsmaildipak@gmail.com>
2024-03-15 22:21:11 +0530

    declaring all the variables, and adding an event listner to rules Btn

--- DiceGame/script.js
+++ DiceGame/script.js
@@ -1,3 +1,22 @@
 const listOfAllDice = document.querySelectorAll(".die");
 const scoreInputs = document.querySelectorAll("#score-options input");
 const scoreSpans = document.querySelectorAll("#score-options span");
+const currentRoundText = document.getElementById("current-round");
+const currentRoundRollsText = document.getElementById("current-round-rolls");
+const totalScoreText = document.getElementById("total-score");
+const scoreHistory = document.getElementById("score-history");
+const rollDiceBtn = document.getElementById("roll-dice-btn");
+const keepScoreBtn = document.getElementById("keep-score-btn");
+const rulesContainer = document.querySelector(".rules-container");
+const rulesBtn = document.getElementById("rules-btn");
+
+let diceValuesArr = [];
+let isModalShowing = false;
+let score = 0;
+let totalScore = 0;
+let round = 1;
+let rolls = 0;
+
+rulesBtn.addEventListener("click", () => {
+  isModalShowing = !isModalShowing;
+});
