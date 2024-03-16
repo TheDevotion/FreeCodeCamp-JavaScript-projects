a59b4b02cfa11f583836a48aaa7d474dbb58a7cf
Devotion <letsmaildipak@gmail.com>
2024-03-16 23:26:36 +0530

    added the algorithm to generate 6 random numbers

--- DiceGame/script.js
+++ DiceGame/script.js
@@ -17,6 +17,19 @@ let totalScore = 0;
 let round = 1;
 let rolls = 0;
 
+const rollDice = () => {
+  diceValuesArr = [];
+
+  for (let i = 0; i < 5; i++) {
+    const randomDice = Math.floor(Math.random() * 6) + 1;
+    diceValuesArr.push(randomDice);
+  }
+
+  listOfAllDice.forEach((dice, index) => {
+    dice.textContent = diceValuesArr[index];
+  });
+};
+
 rulesBtn.addEventListener("click", () => {
   isModalShowing = !isModalShowing;
 
