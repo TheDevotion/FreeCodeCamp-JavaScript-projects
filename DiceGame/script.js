f2936e1c15871d2d0bee076b82fd6a1869bcb5a1
Devotion <letsmaildipak@gmail.com>
2024-03-18 00:15:53 +0530

    finished the project, built logic for detecting, full house, straights and some final touches

--- DiceGame/script.js
+++ DiceGame/script.js
@@ -97,6 +97,27 @@ const detectFullHouse = (arr) => {
   if (hasThreeOfAKind && hasPair) {
     updateRadioOption(2, 25);
   }
+
+  updateRadioOption(5, 0);
+};
+
+const checkForStraights = (arr) => {
+  const sortedNumbersArr = arr.sort((a, b) => a - b);
+  const uniqueNumbersArr = [...new Set(sortedNumbersArr)];
+  const uniqueNumbersStr = uniqueNumbersArr.join("");
+
+  const smallStraightsArr = ["1234", "2345", "3456"];
+  const largeStraightsArr = ["12345", "23456"];
+
+  if (smallStraightsArr.includes(uniqueNumbersStr)) {
+    updateRadioOption(3, 30);
+  }
+
+  if (largeStraightsArr.includes(uniqueNumbersStr)) {
+    updateRadioOption(4, 40);
+  }
+
+  updateRadioOption(5, 0);
 };
 
 const resetRadioOption = () => {
@@ -139,6 +160,8 @@ rollDiceBtn.addEventListener("click", () => {
     rollDice();
     updateStats();
     getHighestDuplicates(diceValuesArr);
+    detectFullHouse(diceValuesArr);
+    checkForStraights(diceValuesArr);
   }
 });
 
