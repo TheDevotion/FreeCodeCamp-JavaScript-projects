31c8c494e3a7a2a76c79ed2c9dba554eacc1f583
Devotion <letsmaildipak@gmail.com>
2024-03-23 12:23:33 +0530

    did object destructuing

--- FccForumLeaderBoard/script.js
+++ FccForumLeaderBoard/script.js
@@ -9,9 +9,27 @@ const fetchData = async () => {
   try {
     const res = await fetch(forumLatest);
     const data = await res.json();
+    showLatestPosts(data);
   } catch (err) {
     console.log(err);
   }
 };
 
 fetchData();
+
+const showLatestPosts = (data) => {
+  const { topic_list, users } = data;
+  const { topics } = topic_list;
+  postsContainer.innerHTML = topics.map((item) => {
+    const {
+      id,
+      title,
+      views,
+      posts_count,
+      slug,
+      posters,
+      category_id,
+      bumped_at,
+    } = item;
+  });
+};
