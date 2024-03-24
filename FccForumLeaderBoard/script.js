a128c699acbacba8f472ec23798e26833384280a
Devotion <letsmaildipak@gmail.com>
2024-03-24 20:53:38 +0530

    getting the title , replies, views activity etc. to be displayed

--- FccForumLeaderBoard/script.js
+++ FccForumLeaderBoard/script.js
@@ -5,6 +5,28 @@ const avatarUrl = "https://sea1.discourse-cdn.com/freecodecamp";
 
 const postsContainer = document.getElementById("posts-container");
 
+const timeAgo = (time) => {
+  const currentTime = new Date();
+  const lastPost = new Date(time);
+
+  const timeDifference = currentTime - lastPost;
+  const msPerMinute = 1000 * 60;
+
+  const minutesAgo = Math.floor(timeDifference / msPerMinute);
+  const hoursAgo = Math.floor(minutesAgo / 60);
+  const daysAgo = Math.floor(hoursAgo / 24);
+
+  if (minutesAgo < 60) {
+    return `${minutesAgo}m ago`;
+  }
+
+  if (hoursAgo < 24) {
+    return `${hoursAgo}h ago`;
+  }
+
+  return `${daysAgo}d ago`;
+};
+
 const fetchData = async () => {
   try {
     const res = await fetch(forumLatest);
@@ -20,16 +42,30 @@ fetchData();
 const showLatestPosts = (data) => {
   const { topic_list, users } = data;
   const { topics } = topic_list;
-  postsContainer.innerHTML = topics.map((item) => {
-    const {
-      id,
-      title,
-      views,
-      posts_count,
-      slug,
-      posters,
-      category_id,
-      bumped_at,
-    } = item;
-  });
+
+  postsContainer.innerHTML = topics
+    .map((item) => {
+      const {
+        id,
+        title,
+        views,
+        posts_count,
+        slug,
+        posters,
+        category_id,
+        bumped_at,
+      } = item;
+
+      return `
+    <tr>
+      <td>
+        <p class="post-title">${title}</p>
+      </td>
+      <td></td>
+      <td>${posts_count - 1}</td>
+      <td>${views}</td>
+      <td>${timeAgo(bumped_at)}</td>
+    </tr>`;
+    })
+    .join("");
 };
