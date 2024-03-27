ee229f26afbffd56f2ab2730756dbee60cf84782
Devotion <letsmaildipak@gmail.com>
2024-03-28 00:21:12 +0530

    getting all the elements

---
+++ PokemonSearchApp/script.js
@@ -0,0 +1,21 @@
+// getting all the elements:
+
+// search elements:
+const searchInput = document.getElementById("search-input");
+const searchButton = document.getElementById("search-button");
+
+//display elements:
+const pokemonName = document.getElementById("pokemon-name");
+const pokemonId = document.getElementById("pokemon-id");
+const weight = document.getElementById("weight");
+const height = document.getElementById("height");
+const types = document.getElementById("types");
+
+// stats elements:
+
+const hp = document.getElementById("hp");
+const attack = document.getElementById("attack");
+const defense = document.getElementById("defense");
+const specialAttack = document.getElementById("special-attack");
+const specialDefense = document.getElementById("special-defense");
+const speed = document.getElementById("speed");
